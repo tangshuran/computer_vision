19103fd0014bc583d59158202f63fca9131a7e13
tang_777 <tangshuran2@gmail.com>
2016-08-19 23:27:34 +0200

    ok

--- test2.py
+++ test2.py
@@ -8,9 +8,18 @@ import numpy as np
 #cv2.imshow("image",img)
 #lena=cv2.imread("lena.png",cv2.COLORMAP_PINK)
 #cv2.imshow("lena",lena)
-left=cv2.imread("left.png")
-right=cv2.imread("right.png")
-add=cv2.add(left/2,right/2)
-cv2.imshow("add",add)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#add
+#left=cv2.imread("left.png")
+#right=cv2.imread("right.png")
+#add=cv2.add(left/2,right/2)
+#cv2.imshow("add",add)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+#add logo
+img1 = cv2.imread('panda2.jpg')
+img2 = cv2.imread('opencv.jpg')
+rows,cols,channels = img2.shape
+roi = img1[0:rows, 0:cols ]
+img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
